5cb5dc053b68acbfe46137154df41ee4ddaf35cc
Ho Kim <great.ho.kim@gmail.com>
2021-10-18 14:27:12 +0900

    Rename project name of client

--- lib/screens/dashboard/components/file_info_card.dart
+++ lib/screens/dashboard/components/file_info_card.dart
@@ -1,8 +1,8 @@
-import 'package:admin/models/MyFiles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../constants.dart';
+import '../../../models/MyFiles.dart';
 
 class FileInfoCard extends StatelessWidget {
   const FileInfoCard({
--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,10 +1,9 @@
-import 'package:admin/responsive.dart';
-import 'package:admin/screens/dashboard/components/my_fields.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants.dart';
+import '../../responsive.dart';
+import 'components/my_fields.dart';
 import 'components/header.dart';
-
 import 'components/recent_files.dart';
 import 'components/storage_details.dart';
 
